9b7dfad147dfbd7b3f7764bb82c83730f9f96ba7
Cell <longchunlong@gmail.com>
2023-03-08 14:52:22 +0800

    练习

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,14 +6,6 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    'gatsby-transformer-remark',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/data/`,
-        fastHash: true,
-      }
-    }
+    'gatsby-transformer-remark'
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,19 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
-import TopNavigationBar from '../components/top_navigation_bar'
 
 // Step 2: Define your component
 const IndexPage = () => {
-  return (
-    <TopNavigationBar>
-      <h1>欢迎来到我的Gatsby站点!</h1>
-      <p>我正在通过gatsby的教程创建网页.</p>
-      <Link to="/about">这里是关于页面</Link>
-    </TopNavigationBar>
-  )
+    return (
+        <main>
+            <h1>欢迎来到我的Gatsby站点!</h1>
+            <p>我正在通过gatsby的教程创建网页.</p>
+        </main>
+    )
 }
 
 // You'll learn about this in the next task, just copy it for now
 export const Head = () => {
-  return (
-    <>
-      <title>主页</title>
-      <meta name="描述" content="这是我自己写的网站" />
-    </>
-  )
+    return <title>Cell的个人网站</title>
 }
 
 // Step 3: Export your component
